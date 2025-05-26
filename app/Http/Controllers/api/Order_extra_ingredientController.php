d3e040f8c8fe327a23f3eeab7d53febea100bd89
juanposada25 <juandiegoposada245@gmail.com>
2025-05-25 19:27:59 -0500

    se creo el create de order_extra_ingredient

--- app/Http/Controllers/api/Order_extra_ingredientController.php
+++ app/Http/Controllers/api/Order_extra_ingredientController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\OrderExtraIngredient;
+use Illuminate\Support\Facades\DB;
 
 class Order_extra_ingredientController extends Controller
 {
@@ -12,7 +14,13 @@ class Order_extra_ingredientController extends Controller
      */
     public function index()
     {
-        //
+            $items = DB::table('order_extra_ingredient')
+            ->join('orders', 'order_extra_ingredient.order_id', '=', 'orders.id')
+            ->join('extra_ingredients', 'order_extra_ingredient.extra_ingredient_id', '=', 'extra_ingredients.id')
+            ->select('order_extra_ingredient.*', 'orders.id as order_id', 'extra_ingredients.name as ingredient_name')
+            ->get();
+
+        return json_encode(['items' => $items]);
     }
 
     /**
@@ -20,7 +28,23 @@ class Order_extra_ingredientController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'order_id' => ['required', 'numeric', 'min:1'],
+            'extra_ingredient_id' => ['required', 'numeric', 'min:1'],
+            'quantity' => ['required', 'numeric', 'min:1'],
+        ]);
+
+        if ($validated->fails()) {
+            return json_encode(['msj' => 'Error de validación', 'statuscode' => 400]);
+        }
+
+        $item = new OrderExtraIngredient();
+        $item->order_id = $request->order_id;
+        $item->extra_ingredient_id = $request->extra_ingredient_id;
+        $item->quantity = $request->quantity;
+        $item->save();
+
+        return json_encode(['item' => $item]);
     }
 
     /**
@@ -28,7 +52,12 @@ class Order_extra_ingredientController extends Controller
      */
     public function show(string $id)
     {
-        //
+         $item = OrderExtraIngredient::find($id);
+        if (is_null($item)) {
+            return abort(404);
+        }
+
+        return json_encode(['item' => $item]);
     }
 
     /**
