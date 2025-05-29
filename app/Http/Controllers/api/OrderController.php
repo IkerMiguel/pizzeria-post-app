fd979b7f171f8dd94b758ff75b34e0b00dbbbf60
IkerMiguel <ikmiguel03@gmail.com>
2025-05-28 21:05:27 -0500

    Add Get Orders Api

--- app/Http/Controllers/api/OrderController.php
+++ app/Http/Controllers/api/OrderController.php
@@ -14,7 +14,21 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = DB::table('orders')   
+        $orders = DB::table('orders')
+            ->join('clients', 'orders.client_id', '=', 'clients.id')
+            ->join('users as client_users', 'clients.user_id', '=', 'client_users.id')
+            ->join('branches', 'orders.branch_id', '=', 'branches.id')
+            ->leftJoin('employees', 'orders.delivery_person_id', '=', 'employees.id')
+            ->leftJoin('users as employees_users', 'employees.user_id', '=', 'employees_users.id')
+            ->select(
+                'orders.id',
+                'client_users.name as client_name',
+                'branches.name as branch_name',
+                'orders.total_price',
+                'orders.status',
+                'orders.delivery_type',
+                'employees_users.name as employees_name'
+            )
             ->get();
         return json_encode(['orders' => $orders]);
     }
