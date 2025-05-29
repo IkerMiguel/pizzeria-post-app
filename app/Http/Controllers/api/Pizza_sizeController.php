298fa7c6cf44bf2290bb631e475b2662791926e2
Emilee <pinzonloaiza06@gmail.com>
2025-05-28 22:00:04 -0500

    actualizacion

--- app/Http/Controllers/api/Pizza_sizeController.php
+++ app/Http/Controllers/api/Pizza_sizeController.php
@@ -11,26 +11,72 @@ class Pizza_sizeController extends Controller
 {
     public function index()
     {
-        //
+         $items = DB::table('pizza_size')
+            ->join('pizzas', 'pizza_size.pizza_id', '=', 'pizzas.id')
+            ->select('pizza_size.*', 'pizzas.name as pizza_name')
+            ->get();
+
+        return response()->json(['items' => $items]);
+
     }
 
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'pizza_id' => ['required', 'numeric'],
+            'size' => ['required', 'in:pequeña,mediana,grande'],
+            'price' => ['required', 'numeric'],
+        ]);
+
+        $item = Pizza_size::create($validated);
+
+        return response()->json(['item' => $item]);
     }
 
     public function show(string $id)
     {
-        //
+        $item = Pizza_size::find($id);
+        if (is_null($item)) {
+            return abort(404);
+        }
+
+        return response()->json(['item' => $item]);
+
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        $validated = $request->validate([
+            'pizza_id' => ['required', 'numeric'],
+            'size' => ['required', 'in:pequeña,mediana,grande'],
+            'price' => ['required', 'numeric'],
+        ]);
+
+        $item = Pizza_size::find($id);
+        if (is_null($item)) {
+            return abort(404);
+        }
+
+        $item->update($validated);
+
+        return response()->json(['item' => $item]);
     }
 
     public function destroy(string $id)
     {
-        //
+        $item = Pizza_size::find($id);
+        if (is_null($item)) {
+            return abort(404);
+        }
+
+        $item->delete();
+
+        $items = DB::table('pizza_size')
+            ->join('pizzas', 'pizza_size.pizza_id', '=', 'pizzas.id')
+            ->select('pizza_size.*', 'pizzas.name as pizza_name')
+            ->get();
+
+        return response()->json(['items' => $items, 'success' => true]);
+
     }
 }
